67c5a7ad1dac2b91783ba9bfac4f1cb3ba714e98
Mario Barbato <mario_barbato@hotmail.com>
2019-09-06 11:15:49 +0200

    added .rd

--- R/AIDmixture.R
+++ R/AIDmixture.R
@@ -2,12 +2,13 @@
 #'
 #' @param colourADMX file name for the colour file
 #' @param Kseq Vector of K to be plotted
+#' @param palette Colour palette
 #' @keywords Admixture_colour
 #' @export
 #' @examples
 #' \dontrun{Admixture_ModPlot("Q_file", "MAP_file.map", "Sort_file.txt", 2:2)}
 
-ColourFile <- function(colourADMX, Kseq){
+ColourFile <- function(colourADMX, Kseq, palette){
   
   # check if colour file exist
   colFile <- NULL
@@ -43,11 +44,10 @@ ColourFile <- function(colourADMX, Kseq){
   }
 }
 
-
 #' Funtion to generate the Admixture PLot
 #'
 #' @param Q_is .Q file stem
-#' @param Map_is .map/fam file
+#' @param Fam_is fam file
 #' @param Sort_is Order in which the populations should be plotted
 #' @param Col_is Filename for the Colour file
 #' @param Kseq Vector of K to be plotted
@@ -62,8 +62,8 @@ ColourFile <- function(colourADMX, Kseq){
 #' \dontrun{Admixture_ModPlot("Q_file", "MAP_file.map", "Sort_file.txt", 2:2)}
 
 plotAdmixture_MB2 <- function(
-  Q_is = "",
-  Map_is = "",
+  Q_is,
+  Fam_is,
   Sort_is = "",
   Col_is = NULL,
   Kseq = 2:4,
@@ -74,11 +74,11 @@ plotAdmixture_MB2 <- function(
   KtoMod = 0
 ){
   
-  if(toPDF) pdf(paste(Q_is, ".ADMX.pdf", sep =""), paper = "a4r" )
+  if(toPDF) pdf(paste(Q_is, "ADMX.pdf", sep =""), paper = "a4r" )
   
-  if(nchar(Map_is) == 0) MAP_is <- paste(Q_is, ".fam", sep = "")
+  #if(nchar(Map_is) == 0) MAP_is <- paste(Q_is, ".fam", sep = "")
   
-  Map <- fread(Map_is, select = 1, col.names = "IDs")
+  Fam <- fread(Fam_is, select = 1, col.names = "IDs")
   SortL <- fread(Sort_is, header = F)
   
   nR = NROW(Kseq)
@@ -93,8 +93,8 @@ plotAdmixture_MB2 <- function(
   
   # sort POPS and plot bars
   for(K in Kseq){
-    Q <- fread(paste(Q_is, ".", K, ".Q", sep = ""))
-    Q <- cbind(Map$IDs, Q)
+    Q <- fread(paste(Q_is, K, ".Q", sep = ""))
+    Q <- cbind(Fam$IDs, Q)
     colnames(Q)[1] <- "IDs"
     Q <- Q[order(factor(Q$IDs, levels = SortL$V1)),]
     
@@ -109,15 +109,15 @@ plotAdmixture_MB2 <- function(
   
   plot(NA, xlim = c(0, NROW(Q)), ylim = c(0,1), axes = F)
   
-  MapU <- data.frame(IDs = unique(Q$IDs), Start = famChange)
+  FamU <- data.frame(IDs = unique(Q$IDs), Start = famChange)
   
-  for(S in 1:NROW(MapU)){
+  for(S in 1:NROW(FamU)){
     
-    oldV = MapU$Start[S] + (ifelse(S != NROW(MapU), MapU$Start[S+1], NROW(Q)) -  MapU$Start[S])/2
+    oldV = FamU$Start[S] + (ifelse(S != NROW(FamU), FamU$Start[S+1], NROW(Q)) -  FamU$Start[S])/2
     
     is.odd <- if(S %% 2) 1 else 1.1
     
-    text(oldV, 1, adj = c(is.odd, is.odd), MapU$IDs[S], srt = 60, cex = 1-(1-1/nR^0.05), xpd = T)
+    text(oldV, 1, adj = c(is.odd, is.odd), FamU$IDs[S], srt = 60, cex = 1-(1-1/nR^0.05), xpd = T)
   }
   
   # ADD EXTRA LINE IF MODDING IS ON
@@ -125,7 +125,6 @@ plotAdmixture_MB2 <- function(
     
     plot(NA, xlim = c(0, KtoMod), ylim = c(0,1), axes = F, xlab = "", ylab = "")
     text(0, 0.5, KtoMod, font = 2, pos = 2)
-    #rect(0:(KtoMod-1), 0, 1:KtoMod, 1, col = ColourPalette[unlist(colFile[KtoMod, 1:KtoMod])])
     rect(0:(KtoMod-1), 0, 1:KtoMod, 1, col = colorPal[1:KtoMod])
   }
   
@@ -136,7 +135,7 @@ plotAdmixture_MB2 <- function(
 #' Main funtion to generate and modify the Admixture PLot
 #'
 #' @param Q_file .Q file stem
-#' @param Map_file .map/fam file
+#' @param fam_file .fam file
 #' @param Sort_file Order in which the populations should be plotted
 #' @param Kseq Vector of K to be plotted
 #' @param ColourPalette A colour palette
@@ -149,9 +148,9 @@ plotAdmixture_MB2 <- function(
 #' \dontrun{Admixture_ModPlot("Q_file", "MAP_file.map", "Sort_file.txt", 2:2)}
 
 Admixture_ModPlot <- function(
-  Q_file = "",
-  Map_file = "",
-  Sort_file = "",
+  Q_file,
+  fam_file = NULL,
+  Sort_file = NULL,
   Kseq = 2:2,
   ColourPalette = NULL,
   mod_plot = F,
@@ -159,22 +158,35 @@ Admixture_ModPlot <- function(
   ToPDF = F
 ){
   
-  stopifnot(file.exists(Q_file) & file.exists(Map_file) & file.exists(Sort_file))
+  # check input files ------
   
-  ColFile.name <- paste(Q_file, "ADMXcolors", sep = ".")
-  # checks or creates colour file
-  ColourFile(ColFile.name, Kseq)
-  # readS it
-  ColFile <- fread(ColFile.name, header = F)
+  # Q files
+  for(k in Kseq) {
+    if(!file.exists(paste(Q_file, k, ".Q", sep = ""))) stop(paste("ERROR: could not open ", Q_file, k, ".Q", sep = ""))
+  }
   
+  # check Fam file
+  if(is.null(fam_file)) fam_file <- paste(Q_file, "fam", sep = "")
+  if(!file.exists(fam_file)) 
+    stop(paste("ERROR: could not open ", fam_file, sep = ""))
+  
+  # check Sort_file
+  if(is.null(Sort_file)) fwrite(unique(fread(fam_file, select = 1)), paste(Q_file, "sort", sep = ""), col.names = F)
+
   # generate color palette if none is provided
   if(is.null(ColourPalette)){
     qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
     ColourPalette = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
   }
+    
+  ColFile.name <- paste(Q_file, "ADMXcolors", sep = "")
+  # checks or creates colour file
+  ColourFile(ColFile.name, Kseq, ColourPalette)
+  # readS it
+  ColFile <- fread(ColFile.name, header = F)
   
   # plot
-  plotAdmixture_MB2(Q_is = Q_file, Map_is = Map_file, Sort_is = Sort_file,
+  plotAdmixture_MB2(Q_is = Q_file, Fam_is = fam_file, Sort_is = Sort_file,
                     Col_is = ColFile, colorPal = ColourPalette,
                     Kseq, modding = mod_plot, KtoMod = KtoMod, toPDF = ToPDF & !mod_plot)
   
@@ -196,8 +208,9 @@ Admixture_ModPlot <- function(
       col2 = as.character(ceiling(coords$x[2]))
       
       if(col1 != col2){
-        cat('swap:', ColourPalette[as.numeric(col1)], "(",col1,")",
-            '&', ColourPalette[as.numeric(col2)], "(",col2,")\n")
+        cat('swap:', ColourPalette[as.numeric(col1)], #"(",col1,")",
+            '&', ColourPalette[as.numeric(col2)]#, "(",col2,")\n"
+            )
         
         # swap colours in the colour set and plot again
         cols <- replace(cols, c(which(cols == col1), which(cols == col2)), c(col2, col1))
@@ -206,12 +219,12 @@ Admixture_ModPlot <- function(
         
         fwrite(ColFile, ColFile.name, sep = '\t', col.names = F)
         
-        plotAdmixture_MB2(Q_is = Q_file, Map_is = Map_file, Sort_is = Sort_file,
+        plotAdmixture_MB2(Q_is = Q_file, Fam_is = fam_file, Sort_is = Sort_file,
                           Col_is = ColFile, colorPal = ColourPalette,
                           Kseq, modding = mod_plot, KtoMod = KtoMod, toPDF = ToPDF)
         
       } else {
-        cat("WARNING: same colour selected, no need to swap.\n")
+        warning("WARNING: same colour selected, no need to swap.")
       }
     } else {
       cat("WARNING: coordinates out of reach\n")
